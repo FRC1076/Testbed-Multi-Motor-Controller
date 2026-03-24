dae3b67dcbeab78c994ee320bc32722a7efc1873
IAmAParabola <407974@aaps.k12.mi.us>
2026-03-23 20:01:15 -0400

    Pin Name Change feather_rp2040.py

--- feather_rp2040.py
+++ feather_rp2040.py
@@ -1,4 +1,4 @@
 MASTER_SWITCH_PIN = board.D9
-PWM_PINS = [board.D24,board.D25]
-ANALOG_PINS = [(board.A0,board.D4),(board.A1,board.RX)]
+PWM_OUT_PINS = [board.D24,board.D25]
+POTENTIOMETER_AND_SWITCH_PINS = [(board.A0,board.D4),(board.A1,board.RX)]
 NEOPIXEL_PIN = board.D6
