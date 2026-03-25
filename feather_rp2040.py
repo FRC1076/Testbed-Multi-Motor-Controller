d63b735bea4ef572608bc624ac0caef56869644f
IAmAParabola <407974@aaps.k12.mi.us>
2026-03-25 13:24:50 -0400

    Number of Lights and Channels added to HW specs feather_rp2040.py

--- feather_rp2040.py
+++ feather_rp2040.py
@@ -1,6 +1,13 @@
 import board
 
+# Pins
 MASTER_SWITCH_PIN = board.D9
 PWM_OUT_PINS = [board.D24,board.D25]
 POTENTIOMETER_AND_SWITCH_PINS = [(board.A0,board.D4),(board.A1,board.RX)]
 NEOPIXEL_PIN = board.D6
+
+# Numbers of things
+NUM_LIGHTS = 32
+NUM_CHANNELS = 2  # NUM_CHANNELS should be a factor of NUM_LIGHTS
+if NUM_CHANNELS > NUM_LIGHTS / 2:
+    raise
