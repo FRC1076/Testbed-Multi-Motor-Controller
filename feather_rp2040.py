65108fe2b6749dc96c45139a370d4a19419fe34a
IAmAParabola <407974@aaps.k12.mi.us>
2026-04-08 20:03:45 -0400

    Light Order Addition feather_rp2040.py

--- feather_rp2040.py
+++ feather_rp2040.py
@@ -15,3 +15,4 @@ if NUM_CHANNELS > NUM_LIGHTS:
 # Other specifications
 DISPLAY_BRIGHTNESS = 0.1
 PWM_FREQUENCY = 50
+LIGHTS_ORDER = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
