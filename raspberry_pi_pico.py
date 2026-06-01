2f59ee5cef8bb4abb33984249e510a95c8cfc658
IAmAParabola <407974@aaps.k12.mi.us>
2026-06-01 19:04:00 -0400

    Formatting Fix raspberry_pi_pico.py
    Made it so you can copy-paste the code and not cause immediate errors due to variables not being defined

--- raspberry_pi_pico.py
+++ raspberry_pi_pico.py
@@ -9,8 +9,8 @@ PWM_OUT_PINS = [board.GP14,board.GP2]
 POTENTIOMETER_AND_SWITCH_PINS = [(board.GP26_A0,board.GP18),(board.GP27_A1,board.GP19)]
 INDICATOR_LIGHT_PIN = board.LED
 NEOPIXEL_PIN = board.GP22
-# OLED_SCL_PIN = board.GP9
-# OLED_SDA_PIN = board.GP8
+OLED_SCL_PIN = board.GP9
+OLED_SDA_PIN = board.GP8
 
 PWM_FREQUENCY = 50
 
@@ -21,11 +21,11 @@ if NUM_CHANNELS > NUM_LIGHTS:
     raise
 
 # Lights specs
-# OLED_VERTICALS = [24, 40, 52] # Verticals for direction text, speed text, and speed bar respectively
-# OLED_HORIZONTALS = [4,68]
-# OLED_DISPLAY_HEIGHT = 64
-# OLED_DISPLAY_WIDTH = 128
-# OLED_BAR_WIDTH = 56
+OLED_VERTICALS = [24, 40, 52] # Verticals for direction text, speed text, and speed bar respectively
+OLED_HORIZONTALS = [4,68]
+OLED_DISPLAY_HEIGHT = 64
+OLED_DISPLAY_WIDTH = 128
+OLED_BAR_WIDTH = 56
 DISPLAY_TYPES = ["OLED", "NEO_PIXEL", "UART"] # Current options are OLED, NEO_PIXEL, UART
 NEO_PIXEL_DISPLAY_BRIGHTNESS = 0.025
 NEO_PIXEL_LIGHTS_ORDER = [28, 20, 12, 4, 29, 21, 13, 5, 30, 22, 14, 6, 31, 23, 15, 7, 24, 16, 8, 0, 25, 17, 9, 1, 26, 18, 10, 2, 27, 19, 11, 3]
