9a0cbdaca5221bcfe63520548d5ad1637fcc16dd
IAmAParabola <407974@aaps.k12.mi.us>
2026-07-29 08:34:23 -0400

    Superclass Update raspberry_pi_pico.py
    This update turns the displays into superclasses and did some basic framework reworking to alow that. Debugging must still occur, I just wanted to save this and show the progress.

--- raspberry_pi_pico.py
+++ raspberry_pi_pico.py
@@ -1,7 +1,7 @@
 import board
 
 """
-Hardware specifications for the first version of the final product.
+Hardware specifications
 """
 
 # Pins
@@ -15,17 +15,14 @@ OLED_SDA_PIN = board.GP8
 PWM_FREQUENCY = 50
 
 # Numbers of things
-NUM_LIGHTS = 32
 NUM_CHANNELS = 2  # NUM_CHANNELS should be a factor of NUM_LIGHTS
 if NUM_CHANNELS > NUM_LIGHTS:
     raise
 
 # Lights specs
-OLED_VERTICALS = [24, 40, 52] # Verticals for direction text, speed text, and speed bar respectively
-OLED_HORIZONTALS = [4,68]
+DISPLAY_TYPES = ["OLED", "NEO_PIXEL"] # Current options are OLED and NEO_PIXEL
+
+NEO_PIXEL_NUM_LIGHTS = 32
+NEO_PIXEL_LIGHTS_ORDER = [28, 20, 12, 4, 29, 21, 13, 5, 30, 22, 14, 6, 31, 23, 15, 7, 24, 16, 8, 0, 25, 17, 9, 1, 26, 18, 10, 2, 27, 19, 11, 3]
 OLED_DISPLAY_HEIGHT = 64
 OLED_DISPLAY_WIDTH = 128
-OLED_BAR_WIDTH = 56
-DISPLAY_TYPES = ["OLED", "NEO_PIXEL", "UART"] # Current options are OLED, NEO_PIXEL, UART
-NEO_PIXEL_DISPLAY_BRIGHTNESS = 0.025
-NEO_PIXEL_LIGHTS_ORDER = [28, 20, 12, 4, 29, 21, 13, 5, 30, 22, 14, 6, 31, 23, 15, 7, 24, 16, 8, 0, 25, 17, 9, 1, 26, 18, 10, 2, 27, 19, 11, 3]
