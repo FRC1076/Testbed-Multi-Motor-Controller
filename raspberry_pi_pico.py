bed14553c179eb7901df457aece8ff9f02e0dbcb
IAmAParabola <407974@aaps.k12.mi.us>
2026-08-01 09:24:00 -0400

    Superclass Debugging raspberry_pi_pico.py
    I didn't get to the OLED code before this commit.

--- raspberry_pi_pico.py
+++ raspberry_pi_pico.py
@@ -16,13 +16,13 @@ PWM_FREQUENCY = 50
 
 # Numbers of things
 NUM_CHANNELS = 2  # NUM_CHANNELS should be a factor of NUM_LIGHTS
-if NUM_CHANNELS > NUM_LIGHTS:
-    raise
 
 # Lights specs
-DISPLAY_TYPES = ["OLED", "NEO_PIXEL"] # Current options are OLED and NEO_PIXEL
+DISPLAY_TYPES = ["NEO_PIXEL"] # Current options are OLED and NEO_PIXEL
 
 NEO_PIXEL_NUM_LIGHTS = 32
+if NUM_CHANNELS > NEO_PIXEL_NUM_LIGHTS:
+    raise
 NEO_PIXEL_LIGHTS_ORDER = [28, 20, 12, 4, 29, 21, 13, 5, 30, 22, 14, 6, 31, 23, 15, 7, 24, 16, 8, 0, 25, 17, 9, 1, 26, 18, 10, 2, 27, 19, 11, 3]
 OLED_DISPLAY_HEIGHT = 64
 OLED_DISPLAY_WIDTH = 128
