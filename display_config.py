1d0a9f2d63d4094a019d6ab50e5d0221d6a71f9e
IAmAParabola <407974@aaps.k12.mi.us>
2026-08-01 10:34:11 -0400

    OLED Superclass Debugging display_config.py

--- display_config.py
+++ display_config.py
@@ -6,6 +6,7 @@ elif board.board_id == 'adafruit_feather_rp2040':
 
 # Constants
 OLED_TEXT_HEIGHT = 8
+OLED_TEXT_CENTERING_VALUE = 4
 
 # UART Configuration
 logging_state = True
@@ -23,10 +24,10 @@ OLED_TOP_HEIGHT = int(hw.OLED_DISPLAY_HEIGHT / 4)       # Currently 16
 OLED_BOTTOM_HEIGHT = int(hw.OLED_DISPLAY_HEIGHT * 3 / 4)      # Currently 48
 OLED_NUM_VERTICALS = 3       # Current Verticals for direction text, speed text, and speed bar in that order
 
-OLED_BAR_WIDTH = int(hw.OLED_DISPLY_WIDTH / hw.NUM_CHANNELS) - (OLED_SPACING * 2)      # Currently 56
+OLED_BAR_WIDTH = int(hw.OLED_DISPLAY_WIDTH / hw.NUM_CHANNELS) - (OLED_SPACING * 2)      # Currently 56
 OLED_HORIZONTALS = [None] * hw.NUM_CHANNELS
-OLED_VERTICALS = 
+OLED_VERTICALS = [None] * OLED_NUM_VERTICALS
 for channel in range(hw.NUM_CHANNELS):
-    OLED_HORIZONTALS[channel] = OLED_SPACING + int(channel * hw.OLED_WIDTH / hw.NUM_CHANNELS)       # Currently 4 and 68
+    OLED_HORIZONTALS[channel] = OLED_SPACING + int(channel * hw.OLED_DISPLAY_WIDTH / hw.NUM_CHANNELS)       # Currently 4 and 68
 for i in range(OLED_NUM_VERTICALS):
-    OLED_VERTICALS[i] = OLED_TOP_HEIGHT + ((1 + (i * 2)) * OLED_SPACING) + (i *OLED_TEXT_HEIGHT)       # Currently 24, 40, and 52
+    OLED_VERTICALS[i] = OLED_TOP_HEIGHT + ((1 + (i * 2)) * OLED_SPACING) + (i * OLED_TEXT_HEIGHT)       # Currently 24, 40, and 52
