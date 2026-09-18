ff24dfae58d9314db340dc6677f34c30c0f13a6c
IAmAParabola <407974@aaps.k12.mi.us>
2026-09-18 10:49:44 -0400

    OLED Code to Comment config.py
    Since neither active hardware sets use the OLED display, I turned the OLED code into comments. If you don't do this, the code breaks since the OLED variables aren't defined in the hw files currently.

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ NEO_SAME_NUM_SPEED_PIXELS= (self.hw.NEO_PIXEL_NUM_LIGHTS - (NEO_SAME_SPEED_DIREC
 NEO_SAME_SPEED_START_VAL = NEO_SAME_SPEED_DIRECTION_BAR * INT_HALF_CHANNELS
 NEO_NO_MODE_LIGHTS = [1, 0, 0, 1, 0.5, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0.5, 1, 0, 0, 1]       # 1 is on, 0.5 is blinking, 0 is off
 
-# OLED Configuratoions
+""" # OLED Configurations
 OLED_BORDER_WIDTH = 1
 OLED_SPACING = 4
 OLED_TOP_HEIGHT = int(hw.OLED_DISPLAY_HEIGHT / 4)       # Currently 16
@@ -43,7 +43,7 @@ OLED_VERTICALS = [None] * OLED_NUM_VERTICALS
 for channel in range(hw.NUM_CHANNELS):
     OLED_HORIZONTALS[channel] = OLED_SPACING + int(channel * hw.OLED_DISPLAY_WIDTH / hw.NUM_CHANNELS)       # Currently 4 and 68
 for i in range(OLED_NUM_VERTICALS):
-    OLED_VERTICALS[i] = OLED_TOP_HEIGHT + ((1 + (i * 2)) * OLED_SPACING) + (i * OLED_TEXT_HEIGHT)       # Currently 24, 40, and 52
+    OLED_VERTICALS[i] = OLED_TOP_HEIGHT + ((1 + (i * 2)) * OLED_SPACING) + (i * OLED_TEXT_HEIGHT)       # Currently 24, 40, and 52 """
 
 # Modes
 # Creation of functions with mode names to allow what I thought was simpler selection code
