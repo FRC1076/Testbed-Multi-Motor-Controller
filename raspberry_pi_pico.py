06aefe420e6a020dcf137ead2f6c6ca1155c2914
IAmAParabola <407974@aaps.k12.mi.us>
2026-09-18 10:43:50 -0400

    OLED Code to Comment raspberry_pi_pico.py
    The variables for the OLED display were not comments, and to keep it from breaking when you had only a NeoPixel, you had to make them comments. I made them comments so that doesn't have to be repeated as the 1st production board only has a NeoPixel.

--- raspberry_pi_pico.py
+++ raspberry_pi_pico.py
@@ -1,7 +1,7 @@
 import board
 
 """
-Hardware specifications
+Hardware specifications for the 1st production board
 """
 
 # Pins
@@ -9,8 +9,8 @@ PWM_OUT_PINS = [board.GP2,board.GP14]
 POTENTIOMETER_AND_SWITCH_PINS = [(board.GP27_A1,board.GP19),(board.GP26_A0,board.GP18)]
 INDICATOR_LIGHT_PIN = board.LED
 NEOPIXEL_PIN = board.GP22
-OLED_SCL_PIN = board.GP9
-OLED_SDA_PIN = board.GP8
+# OLED_SCL_PIN = board.GP9     # This is from when I was testing the OLED screen on a breadboard
+# OLED_SDA_PIN = board.GP8     # This is from when I was testing the OLED screen on a breadboard
 MODE_SWITCH_PIN = board.GP11       # Placeholder
 
 PWM_FREQUENCY = 50
@@ -29,5 +29,5 @@ NEO_PIXEL_NUM_LIGHTS = 32
 if NUM_CHANNELS > NEO_PIXEL_NUM_LIGHTS:
     raise       # You can’t display info on all channels if you don’t have at least one light per channel
 NEO_PIXEL_LIGHTS_ORDER = [24, 16, 8, 0, 25, 17, 9, 1, 26, 18, 10, 2, 27, 19, 11, 3, 28, 20, 12, 4, 29, 21, 13, 5, 30, 22, 14, 6, 31, 23, 15, 7]
-OLED_DISPLAY_HEIGHT = 64
-OLED_DISPLAY_WIDTH = 128
+# OLED_DISPLAY_HEIGHT = 64
+# OLED_DISPLAY_WIDTH = 128
